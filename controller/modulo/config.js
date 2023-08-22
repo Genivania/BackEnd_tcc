5a7b0b2771b72fde396b5b7a981ec9c10ab68a22
Genivania Macedo <109962733+Genivania@users.noreply.github.com>
2023-08-22 09:12:59 -0300

    ok

---
+++ controller/modulo/config.js
@@ -0,0 +1,6 @@
+/*****************************************************************************************
+ * Objetivo: Arquivo responsável pelas váriaveis, constantes e funções globais do projeto.
+ * Data: 22/08/2023
+ * Autor: Genivania Macedo
+ * versão: 1.0
+ *****************************************************************************************/
