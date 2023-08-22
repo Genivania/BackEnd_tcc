0d3c847ca1a207c552f5ce671efa6be3e1a39778
Genivania Macedo <109962733+Genivania@users.noreply.github.com>
2023-08-22 08:31:56 -0300

    git add .

---
+++ app.js
@@ -0,0 +1,29 @@
+/*****************************************************************************
+ * Objetivo: Manipulação de dados.
+ * Autor: Genivania Macedo
+ * Data: 22/08/2023
+ * Versão: 1.0
+ ****************************************************************************/
+
+
+/**
+ * caso não for da um npm init
+ * 
+ * Express - dependencia do Node, que permite a integração entre o protocolo http com o código
+ *     npm install express --save
+ * 
+ * Cors - Gerenciador de permissões para o protocolo HTTP
+ *     npm install cors --save
+ * 
+ * Body-parser - É uma dependencia que permite manipular dados enviados pelo body da requisição 
+ *     npm install body-parser --save
+ * 
+ */
+
+/**
+    Para realizar  a conexão com Banco e dados iremos utilizar o PRISMA
+        npm install prisma --save
+        npx prisma
+        npx prisma init
+        npm install @prisma/client
+ */
